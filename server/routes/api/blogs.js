564f57ca5d339d6c24f1408c98ea813a2dc6d789
Emir Mekić <emir.mekic@live.com>
2019-10-21 21:23:11 +0200

    nemam pojma šta da upišem

--- server/routes/api/blogs.js
+++ server/routes/api/blogs.js
@@ -1,24 +1,41 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
 const router = express.Router();
 
 // Get Blogs
 
-router.get('/', async (req, res) => {
-	const blogs = await loadBlogsCollection();
-	res.send(await blogs.find({}).toArray());
+router.get("/", async (req, res) => {
+  const blogs = await loadBlogsCollection();
+  res.send(
+    await blogs
+      .find({})
+      .sort({ createdAt: -1 })
+      .toArray()
+  );
+});
+
+// Add Post
+
+router.post("/", async (req, res) => {
+  const blogs = await loadBlogsCollection();
+  await blogs.insertOne({
+    text: req.body.text,
+    createdAt: new Date(),
+    comments: ""
+  });
+  res.status(201).send();
 });
 
 async function loadBlogsCollection() {
-	const client = await mongodb.MongoClient.connect(
-		'mongodb+srv://admin:admin@vue-zep8s.mongodb.net/test?retryWrites=true&w=majority',
-		{
-			useNewUrlParser: true
-		}
-	);
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://admin:admin@vue-zep8s.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true
+    }
+  );
 
-	return client.db('vue_express').collection('posts');
+  return client.db("vue_express").collection("posts");
 }
 
 module.exports = router;
