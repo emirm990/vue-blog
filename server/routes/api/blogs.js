20140deff3f6ca0ebfce24b9c5221322e0e1825e
Emir Mekić <emir.mekic@live.com>
2019-10-22 13:40:53 +0200

    dependancies fixes

--- server/routes/api/blogs.js
+++ server/routes/api/blogs.js
@@ -24,7 +24,7 @@ router.post("/", async (req, res) => {
     createdAt: new Date(),
     comments: ""
   });
-  res.status(201).send();
+  res.status(201).send("Posted");
 });
 
 async function loadBlogsCollection() {
