8d02e089626ab5c4ac5eb9624e26a2754630526b
Emir Mekić <emir.mekic@live.com>
2019-11-06 12:57:56 +0100

    Update blogsFrontPage.js

--- server/routes/api/blogsFrontPage.js
+++ server/routes/api/blogsFrontPage.js
@@ -1,10 +1,50 @@
 const express = require("express");
 const mongodb = require("mongodb");
-
+const test = require('assert');
 const router = express.Router();
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admindb:Bzr9I04nau05tRzz@vue-zep8s.mongodb.net/test?retryWrites=true&w=majority";
+MongoClient.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+},
+  function (err, client) {
+
+    if (err) {
+      return console.log('Error: could not connect to mongodb')
+    }
+
+    // Use the admin database for the operation
+    var adminDb = client.db('vue_express').admin()
+    // Add the new user to the admin database
+    adminDb.addUser('korisnik', 'korisnik', {
+      roles: [{
+        role: "userAdmin",
+        db: 'vue_express'
+      }]
+    },
+      function (err, result) {
+
+        if (err) {
+          return console.log(err)
+        }
+
+        // Authenticate using the newly added user
+        adminDb.authenticate(userName, userPassword, function (err, result) {
+
+          if (err) {
+            return console.log('Error: could authenticate with created user')
+          }
+
+          console.log('Ok')
+          db.close()
+        })
+      })
+  })
+
+
 router.get("/", async (req, res) => {
-  console.log(req.route);
   if (req.query.id) {
     const blogs = await loadBlogsCollectionComment();
     res.send(
